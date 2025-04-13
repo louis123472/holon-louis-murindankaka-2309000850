faa274d33bd6719c6d1e9aa1093d4fadaf8ff6d2
louis123472 <holonlouismurindankaka@gmail.com>
2025-04-13 10:32:11 +0200

    Add files via upload

--- cite.php
+++ cite.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>document</title>
+</head>
 <body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
+    <h1>XAMPP</h1>
+    <p></p>
+        <?php
+        echo "<p>hello world from php... </p>\n";
+        echo "date :" .date('j-m-y-h-i-s');
+        ?>
 </body>
 </html>
